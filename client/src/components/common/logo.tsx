41a48d4f42b5eec3d7a67ba3c544cf87383753e8
Amila Priyankara <amila@wearecuriousminds.com>
2025-03-27 09:09:25 +0530

    update logo

--- client/src/components/common/logo.tsx
+++ client/src/components/common/logo.tsx
@@ -1,5 +1,6 @@
 // import { LogoSVG } from "@/assets/logo-svg";
-import logoImage from "@/assets/TapNGrag-logo.jpg";
+// import logoImage from "@/assets/TapNGrag-logo.jpg";
+import logoImage from "@/assets/TnG_Logo.png";
 import { ReactNode, useState } from "react";
 import { useLocation } from "wouter";
 
