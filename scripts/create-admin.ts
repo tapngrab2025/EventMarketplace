910b063e11cd89acab1f7c48fd154761d1cbcc0e
Amila Priyankara <amila@wearecuriousminds.com>
2026-07-04 20:14:45 +0530

    adding initial users and roles for data implementation

--- scripts/create-admin.ts
+++ scripts/create-admin.ts
@@ -5,8 +5,19 @@ import { eq } from "drizzle-orm";
 
 async function createAdmin() {
   try {
-    const password = "admin123"; // You can change this password
+    const password = "u@#&YB5MubLGM1sr"; 
     const hashedPassword = await hashPassword(password);
+
+    const vendorTap = "vendorTap";
+    const vendorTapPassword = "Bu@W98aFxDm4jsm8";
+    const organizerTap = "organizerTap";
+    const organizerTapPassword = "aFxDm4MubLGvJTer";
+    const customerTap = "customerTap";
+    const customerTapPassword = "wYiD$&81#woi0Rja";
+    const vendorHashedTapPassword = await hashPassword(vendorTapPassword);
+    const organizerHashedTapPassword = await hashPassword(organizerTapPassword);
+    const customerHashedTapPassword = await hashPassword(customerTapPassword);
+
 
     // Check if admin already exists
     const existingAdmin = await db.query.users.findFirst({
@@ -19,12 +30,70 @@ async function createAdmin() {
 
     // Create admin user
     await db.insert(users).values({
-      username: "admin",
+      username: "adminTap",
       password: hashedPassword,
       role: "admin",
       name: "System Admin",
-      email: "admin@mail.com",
+      email: "adminTap@mail.com",
     });
+    //"admin", "vendor", "customer", "organizer"
+
+    // Check if vendor already exists
+    const existingVendorTap = await db.query.users.findFirst({
+      where: eq(users.username, "vendor"),
+    });
+
+    if (existingVendorTap) {
+      process.exit(0);
+    }
+
+    // Create vendor user
+    await db.insert(users).values({
+      username: vendorTap,
+      password: vendorHashedTapPassword,
+      role: "vendor",
+      name: "Vendor",
+      email: "vendorTap@mail.com",
+    });
+
+    // Check if organizer already exists
+    const existingOrganizerTap = await db.query.users.findFirst({
+      where: eq(users.username, "organizer"),
+    });
+
+    if (existingOrganizerTap) {
+      process.exit(0);
+    }
+
+    // Create organizer user
+    await db.insert(users).values({
+      username: organizerTap,
+      password: organizerHashedTapPassword,
+      role: "organizer",
+      name: "Organizer",
+      email: "organizerTap@mail.com",
+    });
+    
+
+    // Check if customer already exists
+    const existingCustomerTap = await db.query.users.findFirst({
+      where: eq(users.username, "customer"),
+    });
+
+    if (existingCustomerTap) {
+      process.exit(0);
+    }
+
+    // Create customer user
+    await db.insert(users).values({
+      username: customerTap,
+      password: customerHashedTapPassword,
+      role: "customer",
+      name: "Customer",
+      email: "customerTap@mail.com",
+    });
+
+
 
     // Initialize feedback settings if not exists
     const existingSettings = await db.query.system_settings.findFirst({
